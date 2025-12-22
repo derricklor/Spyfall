6cf042302c63953ad640de20fdf52c5c982e32c3
Derrick Lor <derricklor@gmail.com>
2025-12-22 12:22:51 -0500

    working on votecard, playerList needs id key, todo edit server.js to include id field in playerList

--- client/src/components/VoteCard.jsx
+++ client/src/components/VoteCard.jsx
@@ -1,20 +1,33 @@
 import Card from './Card';
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import PlayerContext from '../contexts/PlayerContext';
 
 const VoteCard = ({playerList, onVote}) => {
     const playerContextObject = useContext(PlayerContext);
 
+    const [voteFor, setVotedFor] = useState('');
+    //filter out current player from list, as you cannot vote for yourself
+    const [newPlayerList, setNewPlayerList] = useState(playerList);
+
+    newPlayerList.filter(player => player.name !== playerContextObject.playerName);
     return (
         <Card title="Vote for a Spy" className="p-4">
             <div className="space-y-4">
                 {playerList.map((player, index) => (
                     <button
-                        key={index}
-                        onClick={() => onVote(playerContextObject.roomCode, playerContextObject.playerCode, player.name)} //(roomCode, playerCode, votedFor)
-                        className="w-full py-3 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg text-black dark:text-white text-lg font-medium transition duration-150"
+                        key={player.id}
+                        disabled={playerContextObject.playerName === player.name}
+                        onClick={() => {
+                            setVotedFor(player.name)
+                            onVote(playerContextObject.roomCode, playerContextObject.playerCode, player.name)
+                        }} //(roomCode, playerCode, votedFor)
+                        className={`w-full py-3 rounded-lg text-lg font-medium text-[var(--dark)] dark:text-[var(--light)] disabled:cursor-not-allowed 
+                            disabled:bg-[var(--secondary)] dark:disabled:bg-[var(--secondary-dark)] dark:disabled:border dark:disabled:border-[var(--secondary)]
+                            transition duration-200
+                            ${voteFor === player.name ? 'ring-4 ring-[var(--primary)] scale-105' : ''}
+                            bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 `}
                     >
-                        <span className="text-xl">{ player.isHost ? 'Host '+ player.name : player.name}</span>
+                        <span className="text-xl">{player.name}</span>
 
                     </button>
                 ))}
